2f3559dd295dc582d6277a2675284439185dc624
yangjunjie <1135844315@qq.com>
2023-02-23 17:48:21 +0800

    chore: update

--- client/src/core/commands/blog/blogCommand.ts
+++ client/src/core/commands/blog/blogCommand.ts
@@ -5,9 +5,22 @@ const blogCommand: CommandType = {
   name: '个人博客',
   alias: [],
   params: [],
-  options: [],
+  options: [
+    {
+      key: 'vercel',
+      desc: '以vercel域名打开',
+      alias: ['v'],
+      type: 'boolean',
+      defaultValue: false,
+    },
+  ],
   action(options, terminal) {
-    window.open('https://vercel-blog-seven-gamma.vercel.app/')
+    const { _, vercel } = options;
+    if (vercel) {
+      window.open('https://vercel-blog-seven-gamma.vercel.app/');
+    }else{
+      window.open('http://124.223.24.47:5502/')
+    }
   },
 };
 
